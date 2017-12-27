2cd98a19e548c1007632b9ae4997b395626c73ad
Jumpy-Squirrel <jsquirrel_github_9a6d@packetloss.de>
2017-12-27 13:17:36 +0100

    build a war that is still standalone capable

---
+++ run.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+# can also set --spring.config.location=/blabla
+java -jar build/libs/rexis-hotel-booking-0.1.0-SNAPSHOT.war --spring.profiles.active=dev
--- src/main/java/info/rexis/hotelbooking/HotelbookingApplication.java
+++ src/main/java/info/rexis/hotelbooking/HotelbookingApplication.java
@@ -2,9 +2,16 @@ package info.rexis.hotelbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HotelbookingApplication {
+public class HotelbookingApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HotelbookingApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(HotelbookingApplication.class, args);
     }
