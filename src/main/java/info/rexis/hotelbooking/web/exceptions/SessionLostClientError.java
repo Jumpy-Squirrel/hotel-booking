93bde103ae36039c0f1340e168208addf1b794ba
Jumpy-Squirrel <jsquirrel_github_9a6d@packetloss.de>
2017-12-29 17:59:10 +0100

    remove dead code

--- src/main/java/info/rexis/hotelbooking/web/exceptions/SessionLostClientError.java
+++ src/main/java/info/rexis/hotelbooking/web/exceptions/SessionLostClientError.java
@@ -1,14 +1,7 @@
 package info.rexis.hotelbooking.web.exceptions;
 
 public class SessionLostClientError extends RuntimeException {
-    private int httpStatus;
-
     public SessionLostClientError(String message) {
         super(message);
-    }
-
-    public SessionLostClientError(String message, int httpStatus) {
-        super(message);
-        this.httpStatus = httpStatus;
     }
 }
