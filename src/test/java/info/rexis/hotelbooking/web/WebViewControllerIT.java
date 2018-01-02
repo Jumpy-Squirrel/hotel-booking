06dc0082567fd2d237643bb7dadb6e887fbb80e3
Jumpy-Squirrel <jsquirrel_github_9a6d@packetloss.de>
2018-01-02 11:45:43 +0100

    refactoring MockMvc integration tests step 2

--- src/test/java/info/rexis/hotelbooking/web/WebViewControllerIT.java
+++ src/test/java/info/rexis/hotelbooking/web/WebViewControllerIT.java
@@ -4,37 +4,24 @@ import info.rexis.hotelbooking.HotelbookingApplication;
 import info.rexis.hotelbooking.repositories.regsys.exceptions.RegsysClientError;
 import info.rexis.hotelbooking.repositories.regsys.exceptions.RegsysServerError;
 import info.rexis.unscanned.MockRegsysFeignClientConfig;
-import org.hamcrest.CoreMatchers;
+import info.rexis.unscanned.WebMockMvcITHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.util.NestedServletException;
-
-import javax.servlet.http.HttpSession;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles({"test"})
 @SpringBootTest
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = {HotelbookingApplication.class, MockRegsysFeignClientConfig.class})
+@ContextConfiguration(classes = {HotelbookingApplication.class, MockRegsysFeignClientConfig.class, WebMockMvcITHelper.class})
 public class WebViewControllerIT {
     @Autowired
-    private MockMvc mockMvc;
+    private WebMockMvcITHelper helper;
 
     private static final String REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION = "/?id=1&token=lala";
     private static final String REQUEST_PATH_MISSING_PARAMETER = "/?id=2";
@@ -44,157 +31,80 @@ public class WebViewControllerIT {
     private static final String PAGE_MAIN = "/main";
     private static final String PAGE_FORM = "/reservation-form";
     private static final String PAGE_SHOW = "/reservation-show";
+    private static final String REQUEST_FOR_REVISIT_TEST = "/?id=2&token=databasetest";
 
     @Test
     public void shouldReturnMainPage() throws Exception {
-        shouldReturnPageWithGet(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION, "id=\"mainpage\"");
+        helper.shouldReturnPageWithGet(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION, "id=\"mainpage\"");
     }
 
     // MockMvc does not support the global error controller, so test passes when a RegsysServerError has bubbled up
     // anything further would mostly be testing Spring Mvc's error handling
     @Test(expected = RegsysServerError.class)
     public void shouldReturnErrorPageWhenRegsysSaysServerError() throws Exception {
-        try {
-            mockMvc.perform(get(REQUEST_PATH_FOR_SERVER_ERROR).header("Accept", "text/html"));
-        } catch (NestedServletException nested) {
-            throw (Exception) nested.getCause();
-        }
+        helper.performGETAndUnwrapNestedException(REQUEST_PATH_FOR_SERVER_ERROR);
     }
 
     // MockMvc does not support the global error controller, so test passes when a RegsysServerError has bubbled up
     // anything further would mostly be testing Spring Mvc's error handling
     @Test(expected = RegsysClientError.class)
     public void shouldReturnErrorPageWhenRegsysSaysNotOk() throws Exception {
-        try {
-            mockMvc.perform(get(REQUEST_PATH_FOR_CLIENT_ERROR).header("Accept", "text/html"));
-        } catch (NestedServletException nested) {
-            throw (Exception) nested.getCause();
-        }
+        helper.performGETAndUnwrapNestedException(REQUEST_PATH_FOR_CLIENT_ERROR);
     }
 
     // MockMvc does not support the global error controller, so test passes when 400 is returned
     @Test
     public void shouldReturnErrorPageWhenMissingParameter() throws Exception {
-        shouldReturn4xxWithGet(REQUEST_PATH_MISSING_PARAMETER);
+        helper.shouldReturn4xxWithGet(REQUEST_PATH_MISSING_PARAMETER);
     }
 
     @Test
     public void shouldReturnForbiddenPageWhenRegsysSaysOkFalse() throws Exception {
-        shouldReturnPageWithGet(REQUEST_PATH_FOR_DENY, "id=\"forbidden\"");
+        helper.shouldReturnPageWithGet(REQUEST_PATH_FOR_DENY, "id=\"forbidden\"");
     }
 
     @Test
     public void shouldReturnSessionLostPageIfSessionWithoutInfo() throws Exception {
-        shouldReturnPageWithGet(PAGE_MAIN, "id=\"sessionlost\"");
+        helper.shouldReturnPageWithGet(PAGE_MAIN, "id=\"sessionlost\"");
     }
 
     @Test
     public void shouldReturnMainPageAgainWithoutParamsIfSessionHasInfo() throws Exception {
-        obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
-        shouldReturnPageWithGet(PAGE_MAIN, "id=\"mainpage\"", session);
+        helper.obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
+        helper.shouldReturnPageWithGetUsingSession(PAGE_MAIN, "id=\"mainpage\"");
     }
 
     @Test
     public void shouldReturnReservationFormPage() throws Exception {
-        obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
-        shouldReturnPageWithGet(PAGE_FORM, "id=\"formpage\"", session);
+        helper.obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
+        helper.shouldReturnPageWithGetUsingSession(PAGE_FORM, "id=\"formpage\"");
     }
 
     @Test
     public void shouldReturnForbiddenIfReservationFormWithoutInfoInSession() throws Exception {
-        shouldReturnPageWithGet(PAGE_FORM, "id=\"sessionlost\"");
+        helper.shouldReturnPageWithGet(PAGE_FORM, "id=\"sessionlost\"");
     }
 
     @Test
     public void shouldDenyWithoutCsrfToken() throws Exception {
-        obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
-        shouldDenyPostWithoutCsrfToken(PAGE_SHOW);
+        helper.obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
+        helper.shouldDenyPostWithoutCsrfToken(PAGE_SHOW);
     }
 
     @Test
     public void shouldReturnReservationShowPage() throws Exception {
-        obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
-        obtainSessionAndCsrfToken(PAGE_FORM, session);
-        shouldReturnPageWithPost(PAGE_SHOW, "id=\"showpage\"");
+        helper.obtainSession(REQUEST_PATH_THAT_WILL_PUT_INFO_IN_SESSION);
+        helper.obtainSessionAndCsrfTokenUsingSession(PAGE_FORM);
+        helper.shouldReturnPageWithPost(PAGE_SHOW, "id=\"showpage\"");
     }
 
     @Test
     public void shouldBringBackEnteredInfoOnSecondVisit() throws Exception {
-        obtainSession("/?id=2&token=databasetest");
-        obtainSessionAndCsrfToken(PAGE_FORM, session);
-        shouldReturnPageWithPost(PAGE_SHOW, "id=\"showpage\"", "rewuilgfaf");
+        helper.obtainSession(REQUEST_FOR_REVISIT_TEST);
+        helper.obtainSessionAndCsrfTokenUsingSession(PAGE_FORM);
+        helper.shouldReturnPageWithPost(PAGE_SHOW, "id=\"showpage\"", "rewuilgfaf");
 
-        obtainSession("/?id=2&token=databasetest");
-        shouldReturnPageWithGet(PAGE_FORM, "value=\"rewuilgfaf\"", session);
-    }
-
-    private String csrftoken;
-    private HttpSession session;
-
-    private void obtainSession(String requestPath) throws Exception {
-        MvcResult result = mockMvc.perform(get(requestPath))
-                .andExpect(status().isOk())
-                .andReturn();
-        session = result.getRequest().getSession();
-    }
-
-    private void obtainSessionAndCsrfToken(String requestPath, HttpSession httpSession) throws Exception {
-        MvcResult result = mockMvc.perform(get(requestPath).session((MockHttpSession) httpSession))
-                .andExpect(status().isOk())
-                .andReturn();
-        // need to do this because the csrf token also changes the session
-        session = result.getRequest().getSession();
-
-        String content = result.getResponse().getContentAsString();
-
-        Pattern p = Pattern.compile(".*name=\"_csrf\" value=\"([a-z0-9-]+)\".*",
-                Pattern.MULTILINE | Pattern.DOTALL);
-        Matcher m = p.matcher(content);
-        if (m.find()) {
-            csrftoken = m.group(1);
-        } else {
-            throw new RuntimeException("Could not obtain a csrf token.");
-        }
-    }
-
-    private void shouldReturnPageWithGet(String requestPath, String expectedExcerpt) throws Exception {
-        mockMvc.perform(get(requestPath).header("Accept", "text/html"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(CoreMatchers.containsString(expectedExcerpt)));
-    }
-
-    private void shouldReturn4xxWithGet(String requestPath) throws Exception {
-        mockMvc.perform(get(requestPath).header("Accept", "text/html"))
-                .andExpect(status().is4xxClientError());
-    }
-
-    private void shouldReturnPageWithGet(String requestPath, String expectedExcerpt, HttpSession httpSession) throws Exception {
-        mockMvc.perform(get(requestPath).session((MockHttpSession) httpSession).header("Accept", "text/html"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(CoreMatchers.containsString(expectedExcerpt)));
-    }
-
-    private void shouldReturnPageWithPost(String requestPath, String expectedExcerpt, String name3value) throws Exception {
-        mockMvc.perform(
-                post(requestPath)
-                        .param("_csrf", csrftoken)
-                        .param("roomtype", "1")
-                        .param("name3", name3value)
-                        .session((MockHttpSession) session)
-        )
-                .andExpect(status().isOk())
-                .andExpect(content().string(CoreMatchers.containsString(expectedExcerpt)));
-    }
-
-    private void shouldReturnPageWithPost(String requestPath, String expectedExcerpt) throws Exception {
-        shouldReturnPageWithPost(requestPath, expectedExcerpt, "");
-    }
-
-    private void shouldDenyPostWithoutCsrfToken(String requestPath) throws Exception {
-        mockMvc.perform(
-                post(requestPath)
-                        .session((MockHttpSession) session)
-        )
-                .andExpect(status().isForbidden());
+        helper.obtainSession(REQUEST_FOR_REVISIT_TEST);
+        helper.shouldReturnPageWithGetUsingSession(PAGE_FORM, "value=\"rewuilgfaf\"");
     }
 }
--- src/test/java/info/rexis/unscanned/WebMockMvcITHelper.java
+++ src/test/java/info/rexis/unscanned/WebMockMvcITHelper.java
@@ -6,8 +6,10 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.util.NestedServletException;
 
 import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,19 +35,7 @@ public class WebMockMvcITHelper {
                 .andReturn();
 
         if (extractSessionAndCsrfToken) {
-            // need to do this because the csrf token also changes the session
-            session = result.getRequest().getSession();
-
-            String content = result.getResponse().getContentAsString();
-
-            Pattern p = Pattern.compile(".*name=\"_csrf\" value=\"([a-z0-9-]+)\".*",
-                    Pattern.MULTILINE | Pattern.DOTALL);
-            Matcher m = p.matcher(content);
-            if (m.find()) {
-                csrftoken = m.group(1);
-            } else {
-                throw new RuntimeException("Could not obtain a csrf token.");
-            }
+            extractSessionAndCsrfToken(result);
         }
     }
 
@@ -76,5 +66,84 @@ public class WebMockMvcITHelper {
         )
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl(expectedRedirectUrl));
+    }
+
+    public void obtainSession(String requestPath) throws Exception {
+        MvcResult result = mockMvc.perform(get(requestPath))
+                .andExpect(status().isOk())
+                .andReturn();
+        session = result.getRequest().getSession();
+    }
+
+    public void obtainSessionAndCsrfTokenUsingSession(String requestPath) throws Exception {
+        MvcResult result = mockMvc.perform(get(requestPath).session((MockHttpSession) session))
+                .andExpect(status().isOk())
+                .andReturn();
+        extractSessionAndCsrfToken(result);
+    }
+
+    public void shouldReturnPageWithGet(String requestPath, String expectedExcerpt) throws Exception {
+        mockMvc.perform(get(requestPath).header("Accept", "text/html"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(CoreMatchers.containsString(expectedExcerpt)));
+    }
+
+    public void shouldReturn4xxWithGet(String requestPath) throws Exception {
+        mockMvc.perform(get(requestPath).header("Accept", "text/html"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    public void shouldReturnPageWithGetUsingSession(String requestPath, String expectedExcerpt) throws Exception {
+        mockMvc.perform(get(requestPath).session((MockHttpSession) session).header("Accept", "text/html"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(CoreMatchers.containsString(expectedExcerpt)));
+    }
+
+    public void shouldReturnPageWithPost(String requestPath, String expectedExcerpt, String name3value) throws Exception {
+        mockMvc.perform(
+                post(requestPath)
+                        .param("_csrf", csrftoken)
+                        .param("roomtype", "1")
+                        .param("name3", name3value)
+                        .session((MockHttpSession) session)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string(CoreMatchers.containsString(expectedExcerpt)));
+    }
+
+    public void shouldReturnPageWithPost(String requestPath, String expectedExcerpt) throws Exception {
+        shouldReturnPageWithPost(requestPath, expectedExcerpt, "");
+    }
+
+    public void shouldDenyPostWithoutCsrfToken(String requestPath) throws Exception {
+        mockMvc.perform(
+                post(requestPath)
+                        .session((MockHttpSession) session)
+        )
+                .andExpect(status().isForbidden());
+    }
+
+    public void performGETAndUnwrapNestedException(String requestPath) throws Exception {
+        try {
+            mockMvc.perform(get(requestPath).header("Accept", "text/html"));
+        } catch (NestedServletException nested) {
+            throw (Exception) nested.getCause();
+        }
+    }
+
+    private void extractSessionAndCsrfToken(MvcResult result) throws UnsupportedEncodingException {
+        // need to do this because the csrf token also changes the session
+        session = result.getRequest().getSession();
+
+        String content = result.getResponse().getContentAsString();
+
+        Pattern p = Pattern.compile(".*name=\"_csrf\" value=\"([a-z0-9-]+)\".*",
+                Pattern.MULTILINE | Pattern.DOTALL);
+        Matcher m = p.matcher(content);
+        if (m.find()) {
+            csrftoken = m.group(1);
+        } else {
+            throw new RuntimeException("Could not obtain a csrf token.");
+        }
     }
 }
