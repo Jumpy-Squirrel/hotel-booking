1bcac3361cac50e294d94b0429a19e029ddba32a
Jumpy-Squirrel <jsquirrel_github_9a6d@packetloss.de>
2018-01-07 13:51:49 +0100

    fix: forgot to set comment column size

--- src/main/java/info/rexis/hotelbooking/services/dto/ReservationDto.java
+++ src/main/java/info/rexis/hotelbooking/services/dto/ReservationDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Index;
@@ -47,6 +48,7 @@ public class ReservationDto {
 
     private int roomtype;
 
+    @Column(length = 8000)
     private String comments;
 
     private String understood;
